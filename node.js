b076b4f989367751a9cafed2b2d1122d1fadc31b
eshan168 <147669123+eshan168@users.noreply.github.com>
2025-07-10 20:57:18 -0700

    Add files via upload

---
+++ node.js
@@ -0,0 +1,20 @@
+const pong = document.getElementById("Pong")
+const tictactoe = document.getElementById("Tic-Tac-Toe")
+const gravity = document.getElementById("Gravity")
+const website = document.getElementById("Website")
+
+function Pong(){
+    pong.scrollIntoView({behavior: "smooth"})
+}
+
+function TicTacToe(){
+    tictactoe.scrollIntoView({behavior: "smooth"})
+}
+
+function Gravity(){
+    gravity.scrollIntoView({behavior: "smooth"})
+}
+
+function Website(){
+    website.scrollIntoView({behavior: "smooth"})
+}
